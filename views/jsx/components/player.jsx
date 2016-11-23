1592fa803703ba10810a74d1f99a5b16fcd7ba91
Darryl B. Dixon <piecedigitalstudios@gmail.com>
2016-11-23 07:44:14 -0500

    changes

--- views/jsx/components/player.jsx
+++ views/jsx/components/player.jsx
@@ -74,15 +74,13 @@ var PlayerStream = _react2["default"].createClass({
     }, 100);
   },
   componentDidMount: function componentDidMount() {
-    var _this = this;
-
     this.refs.tools ? this.refs.tools.addEventListener("mouseleave", function () {
       // console.log("leave");
-      _this.toggleMenu("close");
+      // this.toggleMenu("close");
     }, false) : null;
   },
   render: function render() {
-    var _this2 = this;
+    var _this = this;
 
     // console.log(this.props);
     var _props2 = this.props;
@@ -132,7 +130,7 @@ var PlayerStream = _react2["default"].createClass({
                     _reactRouter.Link,
                     { title: name, to: "/user/" + name, onClick: function () {
                         togglePlayer("collapse");
-                        _this2.toggleMenu("close");
+                        _this.toggleMenu("close");
                       } },
                     display_name || name,
                     display_name && !display_name.match(/^[a-z0-9\_]+$/i) ? "(" + name + ")" : ""
@@ -153,7 +151,7 @@ var PlayerStream = _react2["default"].createClass({
                   _reactRouter.Link,
                   { to: "/user/" + name, onClick: function () {
                       togglePlayer("collapse");
-                      _this2.toggleMenu("close");
+                      _this.toggleMenu("close");
                     } },
                   display_name || name,
                   display_name && !display_name.match(/^[a-z0-9\_]+$/i) ? "(" + name + ")" : ""
@@ -165,7 +163,7 @@ var PlayerStream = _react2["default"].createClass({
                 _react2["default"].createElement(
                   _reactRouter.Link,
                   { to: "https://twitch.tv/" + name, target: "_blank", onClick: function () {
-                      _this2.toggleMenu("close");
+                      _this.toggleMenu("close");
                     } },
                   "Visit On Twitch"
                 )
@@ -173,32 +171,41 @@ var PlayerStream = _react2["default"].createClass({
               _react2["default"].createElement(
                 "div",
                 { className: "closer", onClick: function () {
-                    _this2.swapOut();
-                    _this2.toggleMenu("close");
+                    _this.swapOut();
+                    _this.toggleMenu("close");
                   } },
                 "Close"
               ),
               _react2["default"].createElement(
                 "div",
-                { className: "refresh-video", onClick: function () {
-                    _this2.refresh("video");
-                    _this2.toggleMenu("close");
-                  } },
-                "Refresh Video"
-              ),
-              _react2["default"].createElement(
-                "div",
-                { className: "refresh-chat", onClick: function () {
-                    _this2.refresh("chat");
-                    _this2.toggleMenu("close");
-                  } },
-                "Refresh Chat"
+                { className: "refresh" },
+                _react2["default"].createElement(
+                  "span",
+                  { className: "title" },
+                  "Refresh"
+                ),
+                _react2["default"].createElement(
+                  "span",
+                  { className: "video", onClick: function () {
+                      _this.refresh("video");
+                      _this.toggleMenu("close");
+                    } },
+                  "Video"
+                ),
+                _react2["default"].createElement(
+                  "span",
+                  { className: "chat", onClick: function () {
+                      _this.refresh("chat");
+                      _this.toggleMenu("close");
+                    } },
+                  "Chat"
+                )
               ),
               _react2["default"].createElement(
                 "div",
                 { className: "put-in-view", onClick: function () {
                     putInView(index);
-                    _this2.toggleMenu("close");
+                    _this.toggleMenu("close");
                   } },
                 "Put In View"
               ),
@@ -206,7 +213,7 @@ var PlayerStream = _react2["default"].createClass({
                 "div",
                 { className: "open-panels", onClick: function () {
                     panelsHandler("open", name);
-                    _this2.toggleMenu("close");
+                    _this.toggleMenu("close");
                   } },
                 "Open Stream Panels"
               ),
@@ -214,7 +221,7 @@ var PlayerStream = _react2["default"].createClass({
                 "div",
                 { className: "follow need-auth", onClick: function () {
                     alertAuthNeeded();
-                    _this2.toggleMenu("close");
+                    _this.toggleMenu("close");
                   } },
                 "Follow ",
                 name
@@ -333,10 +340,10 @@ exports["default"] = _react2["default"].createClass({
     }
   },
   componentDidMount: function componentDidMount() {
-    var _this3 = this;
+    var _this2 = this;
 
     this.rescroll = setInterval(function () {
-      var videoList = _this3.refs.videoList;
+      var videoList = _this2.refs.videoList;
 
       videoList.scrollTop = 0;
     }, 1000);
@@ -345,7 +352,7 @@ exports["default"] = _react2["default"].createClass({
     this.rescroll = null;
   },
   render: function render() {
-    var _this4 = this;
+    var _this3 = this;
 
     var _props3 = this.props;
     var userData = _props3.userData;
@@ -385,9 +392,9 @@ exports["default"] = _react2["default"].createClass({
                 togglePlayer: togglePlayer,
                 panelsHandler: panelsHandler,
                 alertAuthNeeded: alertAuthNeeded,
-                layoutTools: _this4.layoutTools,
-                putInView: _this4.putInView,
-                refreshChat: _this4.refreshChat
+                layoutTools: _this3.layoutTools,
+                putInView: _this3.putInView,
+                refreshChat: _this3.refreshChat
               } });
           }) : null
         ),
@@ -397,7 +404,7 @@ exports["default"] = _react2["default"].createClass({
           dataObject ? dataArray.map(function (channelName, ind) {
             var channelData = dataObject[channelName];
             return _react2["default"].createElement(PlayerStream, { ref: function (r) {
-                return _this4[channelName + "_chat"] = r;
+                return _this3[channelName + "_chat"] = r;
               }, key: channelName, name: channelName, display_name: dataObject[channelName], userData: userData, auth: auth, inView: streamInView === ind, isFor: "chat", methods: {} });
           }) : null
         ),
