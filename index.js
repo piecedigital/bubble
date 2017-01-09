9a02e1ebc7896cebe159ec4268e945b4c032a08d
Darryl B. Dixon <piecedigitalstudios@gmail.com>
2017-01-09 03:13:25 -0500

    v 0.6.1

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var env = {
   MESSAGING_SENDER_ID: "766141212604",
   V_MAJOR: 0,
   V_MINOR: 6,
-  V_PATCH: 0,
+  V_PATCH: 1,
 };
 // would put other environment variables here
 cp.fork("./dist/app.js", [], {
