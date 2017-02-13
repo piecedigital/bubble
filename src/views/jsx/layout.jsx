e980a2911f6bb921b990ee84916bf28aef0055f1
Darryl B. Dixon <piecedigitalstudios@gmail.com>
2017-02-12 20:42:31 -0500

    fixed redirect uri

--- src/views/jsx/layout.jsx
+++ src/views/jsx/layout.jsx
@@ -9,12 +9,12 @@ import { Link, browserHistory as History } from 'react-router';
 import Firebase from "firebase";
 
 let redirectURI, clientID;
-if( typeof location === "object" && !location.host.match("amorrius.net") ) {
-  redirectURI = "http://localhost:8080";
-  clientID ="cye2hnlwj24qq7fezcbq9predovf6yy"
-} else {
+if( typeof location === "object" && location.host.match("amorrius.net") ) {
   redirectURI =`https://${location.host}`;
   clientID = "2lbl5iik3q140d45q5bddj3paqekpbi";
+} else {
+  redirectURI = "http://amorrius.dev";
+  clientID ="cye2hnlwj24qq7fezcbq9predovf6yy"
 }
 console.log(redirectURI, clientID);
 
