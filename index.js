db71e2257e50092fc1395047164897263c36c6e2
Darryl B. Dixon <piecedigitalstudios@gmail.com>
2017-04-20 21:20:42 -0400

    version

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var env = {
   MESSAGING_SENDER_ID: "766141212604",
   V_MAJOR: 0,
   V_MINOR: 14,
-  V_PATCH: 1,
+  V_PATCH: 2,
 };
 // would put other environment variables here
 cp.fork("./dist/app.js", [], {
