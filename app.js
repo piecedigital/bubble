2f6581b7a84f0c794ab0710cafb5c07b53b01705
Darryl B. Dixon <piecedigitalstudios@gmail.com>
2017-05-13 01:13:32 -0400

    certificfate stuff

--- app.js
+++ app.js
@@ -39,19 +39,19 @@ var app = (0, _express2["default"])();
 var PORT = process.env["PORT"] || 8080;
 
 // // let's encrypt
-// const letsEncryptReponse = process.env.CERTBOT_RESPONSE;
-// const letsEncryptReponse2 = process.env.CERTBOT_RESPONSE2;
+var letsEncryptReponse = process.env.CERTBOT_RESPONSE;
+var letsEncryptReponse2 = process.env.CERTBOT_RESPONSE2;
 //
 // // Return the Let's Encrypt certbot response:
-// app.get('/.well-known/acme-challenge/:content', function(req, res) {
-//   if(req.headers.host.match("www")) {
-//     // www.amorrius.net
-//     res.send(letsEncryptReponse2);
-//   } else {
-//     // amorrius.net
-//     res.send(letsEncryptReponse);
-//   }
-// });
+app.get('/.well-known/acme-challenge/:content', function (req, res) {
+  if (req.headers.host.match("www")) {
+    // www.amorrius.net
+    res.send(letsEncryptReponse2);
+  } else {
+    // amorrius.net
+    res.send(letsEncryptReponse);
+  }
+});
 // // end let's encrypt
 
 app.use(_express2["default"]["static"](_path2["default"].join(__dirname, "public")));
